a09e02d46bc27c511b156e30f88b173f5d014a0c
Yin2792 <yinyinhtun2792@gmail.com>
2019-10-17 16:05:22 +0630

    fixed chiller

--- src/chiller-realtime/chillerefficiency.js
+++ src/chiller-realtime/chillerefficiency.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import * as Colors from '../assets/everCommColor'
 import Table from './table'
 import { XAxis, AreaChart,Tooltip, YAxis, Area, CartesianGrid,ResponsiveContainer } from 'recharts';
@@ -11,13 +11,36 @@ const  data = [
     { date: '15 Oct 00:00', uv: 200 },
     { date: '16 Oct 00:00', uv: 300 },
 ];
+function getWindowDimensions() {
+  const { innerWidth: width,innerHeight:height } = window;
+  return {
+    width,
+    height
+  };
+}
+function useWindowDimensions(){
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 const ChillerEfficiency=()=>{
+  const { width,height } = useWindowDimensions();
 return(
-  <div>
-<div className="row pl-3">
+  <div style={{textAlign:'left'}}>
+<div className="row pl-3" >
 <div className="col p-0 m-0">
-<div className="pt-1 pb-3 pl-3" style={{fontSize:"16px",color:`${Colors.text1}`}}>
+<div className="pt-1 pb-3 pl-3" style={{fontSize:"16px",color:`${Colors.text1}`}} >
 Chiller Efficiency
 </div>
 <ResponsiveContainer height={300}>
@@ -30,7 +53,7 @@ margin={{ top: 20, right: 80, left: 20, bottom: 5 }}
  <stop offset="95%" stopColor="#163047" />
  </linearGradient>
   </defs>
-<XAxis dataKey="date" stroke="white" stopColor="yellow"  textAnchor="middle">
+<XAxis dataKey="date" stroke="white" stopColor="yellow"  tick={{angle:`${width}`<1157?-60:0}} dy={`${width}`<1157?40:15} height={`${width}`<1157?100:30}>
  {/* <Label position="insideBottom" value="province" /> */}
 </XAxis>
 <YAxis stroke="white" label={{ value: 'KW/Ton', angle: -90, position: 'insideLeft' ,fill:'#fff'}}/>
