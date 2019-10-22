3915442a13e41b7d8c81f52b1323da54c3d68218
Yin2792 <yinyinhtun2792@gmail.com>
2019-10-22 09:38:52 +0630

    new_chiller

--- src/chiller-realtime/chillerresponsive.js
+++ src/chiller-realtime/chillerresponsive.js
@@ -1,124 +1,30 @@
-import React,{Component} from 'react'
+import React, { Component, useState } from 'react'
 import * as Colors from '../assets/everCommColor'
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,ReferenceLine} from 'recharts'
-import Data from './data';
+import {Data} from './data'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts'
 
-const ChillerResponsive=(props)=>{
-    
-  switch(props.lists){
-  case 0:return(
+const ChillerResponsive = (props) => {
+  console.log(props.check)
+  return (
+    <ResponsiveContainer height={300}>
+      <AreaChart data={props.index == 1 ? Data.data1 : props.index == 2 ? Data.data2 : props.index == 3 ? Data.data3 : props.index == 4 ? Data.data4 : props.index == 5 ? Data.data5 : props.index == 6 ? Data.data6 : Data.data7}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        stroke="none">
+        <defs>
+          <linearGradient id="MyGradient" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="darkgreen" />
+            <stop offset="95%" stopColor="#163047" />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="date" tick={{ fill: "#fff" }} stopColor="yellow"  textAnchor="middle" />
+        <YAxis stroke="#666" tickLine={false} tick={{ fill: "#fff" }}  type="number" 
+        domain={props.index==1?(props.check?['dataMin+0.19','dataMax+1']:[0,'auto']):props.index==2?(props.check?['dataMin+0.19','dataMax+1']:[0,'auto']):props.index==3?(props.check?['dataMin+0.19','dataMax+1']:[0,'auto']):props.index==4?(props.check?['dataMin+0.19','dataMax+1']:[0,'auto']):props.index==5?(props.check?['dataMin+0.19','dataMax+1']:[0,'auto']):props.index==6?(props.check?['dataMin+0.19','dataMax+1']:[0,'auto']):(props.check?['dataMin+0.19','dataMax+1']:[0,'auto'])}/>
+        <Tooltip />
+        <CartesianGrid stroke='rgb(103, 104, 107)' />
+        <Area type='monotone' stroke='#0EB801' fill='#0EB801' dataKey="uv"  strokeWidth="2" fillOpacity="0.8" fill="url(#MyGradient)" dot={false}/>
+      </AreaChart>
+    </ResponsiveContainer>
+  )
 
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data1}>
-            <AreaChart data={Data.data1}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-     );
-    case 1:
-        return(
-
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data2}>
-            <AreaChart data={Data.data2}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-     );
-    
-     case 2:return(
-
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data3}>
-            <AreaChart data={Data.data3}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-     );
-     
-     case 3:return(
-
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data4}>
-            <AreaChart data={Data.data4}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-     );
-     
-     case 4:return(
-
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data5}>
-            <AreaChart data={Data.data5}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-     );
-     case 5:return(
-
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data6}>
-            <AreaChart data={Data.data6}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-     );
-     case 6:return(
-
-        <div className="col p-0 m-0">
-            <ResponsiveContainer height={300} data={Data.data7}>
-            <AreaChart data={Data.data7}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-        
-     );
-    
-  }
-    
-    
 }
 export default ChillerResponsive
--- src/chiller-realtime/realtimechiller.js
+++ src/chiller-realtime/realtimechiller.js
@@ -1,162 +1,162 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import * as Colors from '../assets/everCommColor'
 import ChillerTab from './chillertab'
 import ChillerName from './chillername'
+import './sidenav.css'
 import ChillerResponsive from './chillerresponsive'
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,ReferenceLine} from 'recharts'
-import Data from './data';
-const Chillerlists = ['Evaporator Ouput', 'Chill Water Pump2', 'Evaporator Input', 'Chill Water Pump1', 'Condensor Ouput', 'Power Consumption', 'Condensor Input']
-class RealtimeChiller extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      onClick: false,
-      Values: 'Evaporator Ouput',
-    
-      data:Data.data1
-    }
-    this.HandleClick = this.HandleClick.bind(this)
-  }
-  HandleClick = (e, index) => {
+const Chillerlists = [
+  {
+    id: 1,
+    chillername: 'Evaporator Ouput'
+  },
+  {
+    id: 2,
+    chillername: 'Chill Water Pump2'
+  }, 
+  {
+    id: 3,
+    chillername: 'Evaporator Input'
+  }, {
+    id: 4,
+    chillername: 'Chill Water Pump1'
+  }, 
+  {
+    id: 5,
+    chillername: 'Condensor Ouput'
+  }, {
+    id: 6,
+    chillername: 'Power Consumption'
+  },
+  {
+    id: 7,
+    chillername: 'Condensor Input'
+  },
+
+]
+
+const RealtimeChiller = () => {
+  const [Values, setValues] = useState('Evaporator Ouput');
+  const [Index, setIndex] = useState(1);
+  const [showhide,setShowHide] =useState(true)
+  const HandleClick = (e,key) => {
+  
     e.preventDefault();
-    if(index==1){
-      this.setState({data:Data.data2},()=>{
-            console.log("data",this.state.data)
-          });
-    }
-    
-      // switch(index){
-      //   case 0:this.setState({data:Data.data});
-      //   case 1:this.setState({data:Data.data2},()=>{
-      //     console.log("data",this.state.data)
-      //   });
-      //   case 2:this.setState({data:Data.data3});
-      //   case 3:this.setState({data:Data.data4});
-      //   case 4:this.setState({data:Data.data5});
-      //   case 5:this.setState({data:Data.data6});
-      //   case 6:this.setState({data:Data.data7});
-      //   default:this.setState({data:Data.data1})
-      // }
-    Chillerlists.map((values, key) => {
-      if (key == index) {
-        this.setState({ Values: values})
-      }
-    })
-
+    setValues(Chillerlists[key].chillername);
+    setIndex(Chillerlists[key].id)
 
   }
-  getChart=(data)=>{
-    return (
-      <ResponsiveContainer height={300} data={data}>
-            <AreaChart data={data}
-                  margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-              <CartesianGrid stroke="#AAD0FD22" />
-              <XAxis dataKey="name" stroke="#fff"/>
-              <YAxis stroke="#fff"/>
-              <Tooltip/>
-              <Area type='monotone' dataKey='uv' stroke='#0EB801' fill='#0EB801' />
-            </AreaChart>
-        </ResponsiveContainer>
-    )
+  const HideZindex =()=>{
+    setShowHide(false)
   }
-  render() {
-  console.log(this.state.data)
-    return (
-      <div className="d-flex flex-column" style={{ background: "linear-gradient(-60deg, rgb(16, 24, 65), rgb(32, 43, 96))" }}>
-        <div className="flex-grow-1">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="px-0 col-lg-11">
-                <div className="w-100 h-100 px-2" style={{ position: 'absolute', zIndex: 1, marginTop: '-10px' }}>
-                  <div className="container-fluid h-100 pb-4 mt-3" style={{
-                    background: `${Colors.DetatilColor}`
-                    , borderRadius: '8px',
-                    boxShadow: `${Colors.BoxColor} 1px 1px 4px 2px`,
-                  }}>
-                    <div className="row">
-                      <div className="col-12">
-                        <div className="d-flex py-2 justify-content-between">
-                          <div className="d-flex">
-                            <div className="pt-2" style={{ fontSize: '20px', color: `${Colors.text1}` }}>
-                              Chiller1 Data Detail
+  const SideIcon = (e) => {
+     e.preventDefault();
+     var x= document.getElementById('SideLists')
+     if(x.className === "flexLists")
+     x.className+="responsive"
+     else
+     x.className = "flexLists"
+  }
+  return (
+    <div className="d-flex flex-column" style={{ background: "linear-gradient(-60deg, rgb(16, 24, 65), rgb(32, 43, 96))" }}>
+      <div className="flex-grow-1">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="px-0 col-lg-11 pb-0">
+              <div className="w-100 h-100 px-2" style={{ position: 'absolute', zIndex: 1, marginTop: '-10px', display:showhide?'block':'none'}}>
+                <div className="container-fluid h-100 pb-4 mt-3" style={{
+                  background: `${Colors.DetatilColor} none repeat scroll 0% 0%`
+                  , borderRadius: '8px',
+                  boxShadow: `${Colors.BoxColor} 1px 1px 4px 2px`,
+                }}>
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="d-flex py-2 justify-content-between">
+                        <div className="d-flex">
+                        <div className="mr-2 mt-1 sidebarIcon"  style={{cursor: 'pointer',paddingTop: '8px',paddingLeft: '13px', backgroundColor:'rgb(40, 53, 115)', borderRadius: '6px', width: '40px', height: '40px', color: 'rgb(255, 255, 255)'}} onClick={(e)=>SideIcon(e)}><i className="fa fa-bars"></i></div>
+                          <div className="pt-2" style={{ fontSize: '20px', color: `${Colors.text1}` }}>
+                            Chiller1 Data Detail
                                           </div>
-                          </div>
-                          <div className="pl-4 pr-2" style={{
-                            marginTop: '-10px',
-                            fontSize: '34px',
-                            color: `${Colors.text1}`,
-                            cursor: 'pointer'
-                          }}>
-                            ×
+                        </div>
+                        <div className="pl-4 pr-2" style={{
+                          marginTop: '-10px',
+                          fontSize: '34px',
+                          color: `${Colors.text1}`,
+                          cursor: 'pointer'
+                        }}
+                        onClick={(e) => HideZindex(e)}
+                        
+                        >
+                          ×
                                          </div>
-                        </div>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-lg-3 text-secondary pr-4 pl-3">
-                        <div className="h-100 d-flex flex-column pt-3">
-                          {Chillerlists.map((lists, key) => {
-                            return (<div key={key}>
-                              <div className="pl-4 py-3 " style={{
-                                borderBottom: '1px solid rgba(170, 208, 253, 0.133)',
-                                borderLeft: this.state.Values === lists ? `6px solid ${Colors.text3} ` : '6px solid rgba(255, 255, 255, 0)',
-                                cursor: 'pointer',
-                                color: this.state.Values === lists ? `${Colors.text1}` : `${Colors.text2}`,
-                                fontSize: '14px'
-                              }}
-                                onClick={(e) => this.HandleClick(e, key)}>{lists}</div>
-                            </div>
-                            )
-                          })}
-                        </div>
-                      </div>
-                      <div className="col-lg-9 text-secondary px-2">
-                        <div className="d-flex flex-wrap">
-
-                          {Chillerlists.map((lists, key) => {
-
-                            return (<div key={key}>
-                              <div className="px-2 py-1 mx-1 my-1" style={{
-                                border: '1px solid green',
-                                borderRadius: '5px',
-                                fontSize: '14px',
-                                fontWeight: '400',
-                                color: 'green',
-                                backgroundColor: 'rgba(0,0,0,0)',
-                                cursor: 'default',
-                                fontSize: '14px'
-                              }}
-                              >  {lists}</div>
-                            </div>
-                            )
-                          })}
-
-                        </div>
-                        <ChillerName name={this.state.Values} />
-                        {/* <ChillerResponsive lists={this.state.Index}/> */}
-                        <div className="col p-0 m-0">
-                          {this.getChart(this.state.data)}
-                        </div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <div className="container-fluid">
-                  <div className="d-flex pb-1">
-                    <div style={{ cursor: "pointer", color: `${Colors.text2}` }}>
-                      Plant
-                   <span className="px-1">></span>
-                    </div>
-                    <div style={{ cursor: "pointer", color: `${Colors.text1}` }}>
-                      Chiller
-                 </div>
                   </div>
                   <div className="row">
-                    <div className="col-lg-9 px-0 mx-0 pb-3">
-                      <div className="container-fluid py-0">
-                        <ChillerTab />
+                    <div className="col-lg-3 text-secondary pr-4 pl-3">
+                      <div className="flexLists" id="SideLists">
+                      <div className="h-100 d-flex flex-column pt-3" >
+        
+                        {Chillerlists.map((lists, key) => {
+                          return (<div key={key}>
+                            <div className="pl-4 py-3" style={{
+                              borderBottom: '1px solid rgba(170, 208, 253, 0.133)',
+                              borderLeft: Values === lists.chillername ? `6px solid ${Colors.text3} ` : '6px solid rgba(255, 255, 255, 0)',
+                              cursor: 'pointer',
+                              color: Values === lists.chillername ? `${Colors.text1}` : `${Colors.text2}`,
+                              fontSize: '14px'
+                            }}
+                              onClick={(e) => HandleClick(e,key)}>{lists.chillername}</div>
+                          </div>
+                          )
+                        })}
+                      
                       </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-9 text-secondary px-2">
+                      <div className="d-flex flex-wrap" >
+
+                        {Chillerlists.map((lists, key) => {
+
+                          return (<div key={key}>
+                            <div className="px-2 py-1 mx-1 my-1" style={{
+                              border: '1px solid green',
+                              borderRadius: '5px',
+                              fontSize: '14px',
+                              fontWeight: '400',
+                              color: 'green',
+                              backgroundColor: 'rgba(0,0,0,0)',
+                              cursor: 'default',
+                              fontSize: '14px'
+                            }}
+                            >  {lists.chillername}</div>
+                          </div>
+                          )
+                        })}
+
+                      </div>
+                      <ChillerName name={Values} index = {Index}/>
+                      {/* <ChillerResponsive lists={this.state.Index}/> */}
+                      
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+              <div className="container-fluid">
+                <div className="d-flex pb-1">
+                  <div style={{ cursor: "pointer", color: `${Colors.text2}` }}>
+                    Plant
+                   <span className="px-1">></span>
+                  </div>
+                  <div style={{ cursor: "pointer", color: `${Colors.text1}` }}>
+                    Chiller
+                 </div>
+                </div>
+               < div className="row">
+                  <div className="col-lg-9 px-0 mx-0 pb-3">
+                    <div className="container-fluid py-0">
+                      <ChillerTab />
                     </div>
                   </div>
                 </div>
@@ -165,11 +165,10 @@ class RealtimeChiller extends Component {
           </div>
         </div>
       </div>
+    </div>
 
+  )
 
-
-    )
-
-  }
 }
+
 export default RealtimeChiller
